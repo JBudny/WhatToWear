073865c39cdda144724591ec0011fdb7a1629135
Jbudny <jbudny8@gmail.com>
2018-09-15 00:28:32 +0200

    Fetch weather data based on user city

--- src/App.js
+++ src/App.js
@@ -2,42 +2,44 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 const headerStyle = {
-  'borderBottom': '1px solid black',
-  'marginBottom': '15px'
+  borderBottom: '1px solid black',
+  height: '10%',
+  textAlign: 'center',
+  display: 'flex',
+  justifyContent: 'center',
+  textAlign: 'center'
 }
 const cardStyle = {
-  'display': 'inline-block',
-  'width': '350px',
-  'height': '60vh',
-  'margin': '5px',
-  'border': '1px solid black'
+  display: 'inline-block',
+  width: '350px',
+  height: '60vh',
+  margin: '5px',
+  border: '1px solid black'
 }
 const containerStyle = {
-  'margin': '15px',
-  'display': 'flex',
-  'flexWrap': 'wrap',
-  'justifyContent': 'center',
-  'alignItems': 'center'
+  margin: '15px',
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+  alignItems: 'center',
 }
 
 class Header extends Component {
   render() {
     return (
       <div className="Hheader" style={headerStyle}>
-        <h2>{this.props.heading}</h2>
+        <h2 style={{margin: 'auto'}}>{this.props.heading}</h2>
       </div>
-    )
-  }
+    )}
 }
 
 class Content extends Component {
   render() {
     return (
-      <div className="Content">
+      <div className="Content" style={{height: '90%', width:'100%'}}>
         {this.props.content}
       </div>
-    )
-  }
+    )}
 }
 
 class Card extends Component {
@@ -47,37 +49,60 @@ class Card extends Component {
         <Header heading={this.props.cardName}/>
         <Content content={this.props.cardContent}/>
       </div>
-    )
-  }
+    )}
 }
 
 class InputLocation extends Component {
   render() {
     return (
-      <div>
+      <div style={{textAlign: 'center', margin: '15px'}} >
         <h2>Type your city</h2>
         <input type="text"/>
       </div>
-    )
-  }
+    )}
 }
 
 class Weather extends Component {
+  constructor(props){
+    super(props)
+    this.state = {}
+    this.url = 'https://ipapi.co/json'
+    this.weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
+    this.weatherApiKey = '&APPID=9fc75b96c3e130cffdee8b45127936db&units=metric'
+    fetch(this.url)
+      .then((response) => response.json())
+      .then((ipData) =>
+        fetch(this.weatherUrl+ipData.city+this.weatherApiKey)
+        .then((response) => response.json())
+        .then((weatherData) => this.setState(weatherData))
+      )
+  }
+
   render() {
     return (
-      <Card cardName='Weather info'
-         cardContent=
-         {
-           <ul>
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
+      <Card cardName='Weather info' cardContent={
+            this.state.weather
+            ?
+            <React.Fragment>
+              <span style={{lineHeight: '55px', margin: '5px'}}>Weather icon: </span>
+              <img src={'http://openweathermap.org/img/w/'+this.state.weather[0].icon+'.png'}
+              alt="" style={{ right: '0', top: '0',width:'35px'}}/>
+              <ul style={{listStyleType: 'none', margin: '5px'}}>
+                <li><span>City: </span>{this.state.name}</li>
+                <li><span>Forecast: </span>{this.state.weather[0].description}</li>
+                <li><span>Temperature: </span>{this.state.main.temp}<span>°C</span></li>
+                <li><span>Min temperature: </span>{this.state.main.temp_min}<span>°C</span></li>
+                <li><span>Max temperature: </span>{this.state.main.temp_max}<span>°C</span></li>
+                <li><span>Humidity: </span>{this.state.main.humidity}<span>%</span></li>
+                <li><span>Pressure: </span>{this.state.main.pressure}<span>hPa</span></li>
               </ul>
-        }
-      />
-    )
-  }
+            </React.Fragment>
+            :
+            <div style={{display:'flex',textAlign:'center',justifyContent:'center',flexDirection: 'column'}}>
+              <span style={{display: 'inline', margin:'2px'}}>Loading...</span>
+            </div>
+        }/>
+    )}
 }
 
 class OutfitPicture extends Component {
@@ -93,19 +118,14 @@ class OutfitPicture extends Component {
 class OutfitDescription extends Component {
   render() {
     return (
-      <Card cardName='Description'
-               cardContent=
-               {
-                 <ul>
+      <Card cardName='Description' cardContent=
+               {<ul style={{listStyleType: 'none'}}>
                    <li>1</li>
                    <li>2</li>
                    <li>3</li>
                    <li>4</li>
-                 </ul>
-              }
-      />
-    )
-  }
+                 </ul>}/>
+    )}
 }
 
 class App extends Component {
@@ -113,7 +133,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to WhatToWear</h1>
+          <h1 className="App-title" style={{textAlign: 'center'}} >Welcome to WhatToWear</h1>
         </header>
         <InputLocation/>
         <div className="Container" style={containerStyle}>
