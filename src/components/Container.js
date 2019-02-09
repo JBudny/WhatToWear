4540de78841c80e494ea6f4f370b928e4adfb967
JBudny <jbudny8@gmail.com>
2019-02-09 21:33:35 +0100

    Improve handling of weather data fetching errors and some small code improvements

--- src/components/Container.js
+++ src/components/Container.js
@@ -40,62 +40,84 @@ class Container extends Component {
   constructor() {
     super()
     this.state = {}
-    this.url = 'https://ipapi.co/json'
+    this.ipDataUrl = 'https://ipapi.co/json'
     this.weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?'
     this.weatherApiKey = 'APPID=9fc75b96c3e130cffdee8b45127936db&units=metric'
     this.outfitData = []
   }
 
   componentDidMount() {
-    if ("geolocation" in navigator) {
+    if ("geoloscation" in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
-        this.lat = position.coords.latitude
-        this.lon = position.coords.longitude
-        this.getWeatherByCoords(this.lat, this.lon)
-      }, (error) => this.getWeatherByIP(error))
+        const lat = position.coords.latitude
+        const lon = position.coords.longitude
+        this.getWeatherByCoords(lat, lon)
+      }, (error) => this.getIpData(error))
     }else {
-      this.error = {message: "Your browser doesn't support Geolocation_API"}
-      this.getWeatherByIP(this.error)}
+      const notification = "Your browser doesn't support Geolocation_API."+
+                           "The weather is based on your IP and can be inacurate."
+      this.getIpData(notification)}
     }
 
 getWeatherByCoords = (lat, lon) => {
   fetch(`${this.weatherUrl}lat=${lat}&lon=${lon}&${this.weatherApiKey}`)
-  .then((response) => response.json())
+  .then((weatherData) => weatherData.json())
   .then((weatherData) => this.getOutfitData(weatherData))
+  .catch((error) => this.setState({error}))
 }
 
-  getWeatherByIP = (error) => {
-    fetch('https://ipapi.co/json/')
-    .then((response) => response.json())
+  getIpData = (notification) => {
+    fetch(this.ipDataUrl)
+    .then((ipData) => ipData.json())
     .then((ipData) => {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ipData.city}&${this.weatherApiKey}`)
-      .then((response) => response.json())
-      .then((weatherData) => this.getOutfitData(weatherData))
-      .then(() => {
-        this.errorNotification = {message:''}
-        this.errorNotification.message=`${error.message}. The weather is based on your IP and can be inacurate.`
-        this.setState({error: this.errorNotification})})
+      this.getWeatherByIp(ipData, notification)
+    })
+    .catch((error) => {
+      error = {
+        error,
+        message:error.message+' your ip data. You can try to type your city by hand.',
+        stack:error.stack,notification
+      }
+      this.setState({error})
+    })
+  }
+
+  getWeatherByIp = (ipData, notification) => {
+    fetch(`${this.weatherUrl}q=${ipData.city}&${this.weatherApiKey}`)
+    .then((weatherData) => weatherData.json())
+    .then((weatherData) => this.getOutfitData(weatherData))
+    .then(() => this.setState({notification}))
+    .catch((error) => {
+      error = {
+        error,
+        message:error.message+' weather data.',
+        stack:error.stack,notification
+      }
+      this.setState({error})
     })
   }
 
   getOutfitData = (weatherData) => {
-    this.outfitData = clothesData.filter((object) => {
+    this.outfitData = clothesData.filter((clothes) => {
+      const {main} = weatherData.weather
+      const {temp} = weatherData.main
+      const {rain, maxTemp, minTemp, cloth} = clothes
       if(weatherData.weather){
-        if (weatherData.weather.main !== 'rain' &&
-        weatherData.weather.main !== 'thunderstorm' &&
-        weatherData.weather.main !== 'shower rain') {
-          if (weatherData.main.temp >= object.minTemp && object.rain !== '1') {
-            if (weatherData.main.temp <= object.maxTemp) {
-              return object.cloth
-            }
-          }
-        } else if (weatherData.main.temp >= object.minTemp) {
-            if (weatherData.main.temp <= object.maxTemp) {
-              return object.cloth
-            }
-          }
+        if (main !== 'rain' &&
+            main !== 'thunderstorm' &&
+            main !== 'shower rain') {
+              if (temp >= minTemp && rain !== '1') {
+                if (temp <= maxTemp) {
+                  return cloth
+                }
+              }
+            } else if (temp >= minTemp) {
+                if (temp <= maxTemp) {
+                  return cloth
+                }
+              }
       }
-    return 0;
+    return 0
     })
     this.setState(weatherData)
   }
@@ -114,4 +136,4 @@ render() {
 }
 }
 
-export default Container;
+export default Container
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,36 +1,57 @@
 // module "Weather.js"
-import React from 'react'
+import React, {Fragment} from 'react'
 import Card from './Card'
 
 const Weather = ({weatherData}) => {
+const render = () => (
+  <Fragment>
+    <span style={{lineHeight: '55px',margin: '5px'}}>Weather icon:</span>
+    <img src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="" style={{right: '0',top: '0',width: '35px'}}/>
+    <ul style={{listStyleType: 'none',margin: '5px'}}>
+      <li><span>City:</span>{weatherData.name}</li>
+      <li><span>Forecast:</span>{weatherData.weather[0].description}</li>
+      <li><span>Main:</span>{weatherData.weather[0].main}</li>
+      <li><span>Temperature:</span>{weatherData.main.temp}<span>°C</span></li>
+      <li><span>Min temperature:</span>{weatherData.main.temp_min}<span>°C</span></li>
+      <li><span>Max temperature:</span>{weatherData.main.temp_max}<span>°C</span></li>
+      <li><span>Humidity:</span>{weatherData.main.humidity}<span>%</span></li>
+      <li><span>Pressure:</span>{weatherData.main.pressure}<span>hPa</span></li>
+    </ul>
+    {
+      weatherData.notification
+      ?
+        <div style={{display: 'flex',textAlign: 'left',justifyContent: 'center',flexDirection: 'column'}}>
+          <span style={{display: 'inline',margin: '2px', color:'blue', fontSize:'0.82em'}}>INFO: {weatherData.notification}</span>
+        </div>
+      : null
+    }
+  </Fragment>
+)
+
+const render1 = () => (
+  <Fragment>
+  {
+    weatherData.error
+    ?
+    <div style={{display: 'flex',textAlign: 'left',justifyContent: 'center',flexDirection: 'column'}}>
+      <span style={{display: 'inline',margin: '2px', color:'red', fontSize:'0.82em'}}>CRITICAL ERROR</span>
+      <span style={{display: 'inline',margin: '2px', color:'red', fontSize:'0.82em'}}>MESSAGE: {weatherData.error.message}</span>
+      <span style={{display: 'inline',margin: '2px', color:'red', fontSize:'0.82em'}}>STACK: {weatherData.error.stack}</span>
+    </div>
+    :null
+  }
+  </Fragment>
+)
+
   return (
     <Card cardName='Weather info' cardContent={
       weatherData.weather
-        ? <React.Fragment>
-          <span style={{lineHeight: '55px',margin: '5px'}}>Weather icon:</span>
-          <img src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="" style={{right: '0',top: '0',width: '35px'}}/>
-          <ul style={{listStyleType: 'none',margin: '5px'}}>
-            <li><span>City:</span>{weatherData.name}</li>
-            <li><span>Forecast:</span>{weatherData.weather[0].description}</li>
-            <li><span>Main:</span>{weatherData.weather[0].main}</li>
-            <li><span>Temperature:</span>{weatherData.main.temp}<span>°C</span></li>
-            <li><span>Min temperature:</span>{weatherData.main.temp_min}<span>°C</span></li>
-            <li><span>Max temperature:</span>{weatherData.main.temp_max}<span>°C</span></li>
-            <li><span>Humidity:</span>{weatherData.main.humidity}<span>%</span></li>
-            <li><span>Pressure:</span>{weatherData.main.pressure}<span>hPa</span></li>
-          </ul>
-          {weatherData.error
-            ?
-              <div style={{display: 'flex',textAlign: 'left',justifyContent: 'center',flexDirection: 'column'}}>
-                <span style={{display: 'inline',margin: '2px', color:'red'}}>{weatherData.error.message}</span>
-                <span style={{display: 'inline',margin: '2px'}}>&nbsp;</span>
-              </div>
-            : null}
-        </React.Fragment>
+        ?
+        render(weatherData)
         :
-        <div style={{display: 'flex',textAlign: 'center',justifyContent: 'center',flexDirection: 'column'}}>
-          <span style={{display: 'inline',margin: '2px'}}>Can&#39;t get weather data. You can type your city by hand.</span>
-        </div>}/>)
+        render1(weatherData)
+    }/>
+  )
 }
 
 export default Weather;
