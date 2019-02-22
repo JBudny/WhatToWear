a4f5240eed153db1b95df508fbf563d572ac89ef
JBudny <jbudny8@gmail.com>
2019-02-22 12:51:49 +0100

    +Add Japanese transtlation

--- src/index.js
+++ src/index.js
@@ -3,19 +3,22 @@ import ReactDOM from 'react-dom';
 import { IntlProvider, addLocaleData } from 'react-intl';
 import localeEn from 'react-intl/locale-data/en';
 import localePl from 'react-intl/locale-data/pl';
+import localeJa from 'react-intl/locale-data/ja';
 import messagesEn from './translations/en.json';
 import messagesPl from './translations/pl.json';
+import messagesJa from './translations/ja.json';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './components/App';
 
 const messages = {
   en: messagesEn,
-  pl: messagesPl
+  pl: messagesPl,
+  ja: messagesJa
 };
 const language = navigator.language.split(/[-_]/)[0];
 
-addLocaleData([...localeEn, ...localePl]);
+addLocaleData([...localeEn, ...localePl, ...localeJa]);
 
 ReactDOM.render(
   <IntlProvider locale={language} messages={messages[language]}>
